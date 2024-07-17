96c53b11cff7787ea282306023a350abef511967
trinhdaiphuc <trinhdaiphuc@gmail.com>
2024-07-17 16:50:46 +0700

    add env struct function and write Readme

--- README.md
+++ README.md
@@ -7,3 +7,72 @@
 ```shell
 go get -u github.com/trinhdaiphuc/env_config
 ```
+
+## Usage
+
+Define your config with struct tag `env:{env_variable,default_value}`. Ex:
+
+```go
+type Config struct {
+    Host    string  `env:"HOST,localhost"`
+    Port    int     `env:"PORT,8080"`
+    Bytes   []byte  `env:"ENV_BYTES,foo"`
+    Float32 float32 `env:"ENV_FLOAT"`
+    None    string
+    Timeout time.Duration `env:"TIMEOUT"`
+    Date    time.Time     `env:"ENV_DATE"`
+}
+```
+
+The library will get environment value from `env_variable` if value is empty it will add value with `default_value` you
+describe. Example:
+
+I use `.env` file for define environments easily:
+
+```dotenv
+HOST=127.0.0.1
+PORT=8081
+ENV_BYTES=foo
+ENV_FLOAT=
+TIMEOUT=5s
+ENV_DATE=2021-08-07T15:04:05Z
+```
+
+I will get these environments into my config struct. [example.go](./examples/example.go)
+
+```go
+package main
+
+import (
+	"fmt"
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/trinhdaiphuc/env_config"
+	"time"
+)
+
+type Config struct {
+	Host    string  `env:"HOST,localhost"`
+	Port    int     `env:"PORT,8080"`
+	Bytes   []byte  `env:"ENV_BYTES,foo"`
+	Float32 float32 `env:"ENV_FLOAT,12.34"`
+	None    string
+	Timeout time.Duration `env:"TIMEOUT"`
+	Date    time.Time     `env:"ENV_DATE"`
+}
+
+func main() {
+	cfg := &Config{}
+	if err := env_config.EnvStruct(cfg); err != nil {
+		panic(err)
+	}
+	fmt.Printf("Config %+v\n", cfg)
+}
+```
+
+Now you can run `example.go` to check this
+
+```shell
+go run examples/example.go
+
+Config &{127.0.0.1 8081 [102 111 111] 12.34  5s 2021-08-07 15:04:05 +0000 UTC}
+```
--- env.go
+++ env.go
@@ -2,9 +2,116 @@ package env_config
 
 import (
 	"os"
+	"reflect"
 	"strconv"
 	"time"
 )
+
+func EnvStruct(s interface{}) error {
+	out := Map(s)
+	fields := structVal(s)
+	for fieldName, env := range out {
+		field := fields.FieldByName(fieldName)
+		switch env.Value.(type) {
+		case string:
+			field.SetString(Env(env.Environment, env.DefaultValue))
+		case int:
+			data, err := EnvInt(env.Environment, env.DefaultValue)
+			if err != nil {
+				return err
+			}
+			field.Set(reflect.ValueOf(data))
+		case int8:
+			data, err := EnvInt8(env.Environment, env.DefaultValue)
+			if err != nil {
+				return err
+			}
+			field.Set(reflect.ValueOf(data))
+		case int16:
+			data, err := EnvInt16(env.Environment, env.DefaultValue)
+			if err != nil {
+				return err
+			}
+			field.Set(reflect.ValueOf(data))
+		case int32:
+			data, err := EnvInt32(env.Environment, env.DefaultValue)
+			if err != nil {
+				return err
+			}
+			field.Set(reflect.ValueOf(data))
+		case int64:
+			data, err := EnvInt64(env.Environment, env.DefaultValue)
+			if err != nil {
+				return err
+			}
+			field.Set(reflect.ValueOf(data))
+		case uint:
+			data, err := EnvUint(env.Environment, env.DefaultValue)
+			if err != nil {
+				return err
+			}
+			field.Set(reflect.ValueOf(data))
+		case uint8:
+			data, err := EnvUint8(env.Environment, env.DefaultValue)
+			if err != nil {
+				return err
+			}
+			field.Set(reflect.ValueOf(data))
+		case uint16:
+			data, err := EnvUint16(env.Environment, env.DefaultValue)
+			if err != nil {
+				return err
+			}
+			field.Set(reflect.ValueOf(data))
+		case uint32:
+			data, err := EnvUint32(env.Environment, env.DefaultValue)
+			if err != nil {
+				return err
+			}
+			field.Set(reflect.ValueOf(data))
+		case uint64:
+			data, err := EnvUint64(env.Environment, env.DefaultValue)
+			if err != nil {
+				return err
+			}
+			field.Set(reflect.ValueOf(data))
+		case float32:
+			data, err := EnvFloat32(env.Environment, env.DefaultValue)
+			if err != nil {
+				return err
+			}
+			field.Set(reflect.ValueOf(data))
+		case float64:
+			data, err := EnvFloat64(env.Environment, env.DefaultValue)
+			if err != nil {
+				return err
+			}
+			field.Set(reflect.ValueOf(data))
+		case []byte:
+			data := EnvBytes(env.Environment, env.DefaultValue)
+			field.Set(reflect.ValueOf(data))
+		case bool:
+			data, err := EnvBool(env.Environment, env.DefaultValue)
+			if err != nil {
+				return err
+			}
+			field.Set(reflect.ValueOf(data))
+		case time.Duration:
+			data, err := EnvDuration(env.Environment, env.DefaultValue)
+			if err != nil {
+				return err
+			}
+			field.Set(reflect.ValueOf(data))
+		case time.Time:
+			data, err := EnvTime(env.Environment, env.DefaultValue, "")
+			if err != nil {
+				return err
+			}
+			field.Set(reflect.ValueOf(data))
+		}
+	}
+	return nil
+}
 
 // Env String
 func Env(key, defaultValue string) (value string) {
@@ -21,16 +128,40 @@ func EnvBytes(key, defaultValue string) []byte {
 
 // EnvDuration time Duration
 func EnvDuration(key, defaultValue string) (time.Duration, error) {
-	return time.ParseDuration(Env(key, defaultValue))
+	env := Env(key, defaultValue)
+	if env == "" {
+		return 0, nil
+	}
+	return time.ParseDuration(env)
+}
+
+// EnvTime time
+func EnvTime(key, defaultValue, layout string) (time.Time, error) {
+	env := Env(key, defaultValue)
+	if env == "" {
+		return time.Time{}, nil
+	}
+	if layout == "" {
+		layout = time.RFC3339
+	}
+	return time.Parse(layout, env)
 }
 
 // EnvInt Integer
 func EnvInt(key, defaultValue string) (int, error) {
-	return strconv.Atoi(Env(key, defaultValue))
+	env := Env(key, defaultValue)
+	if env == "" {
+		return 0, nil
+	}
+	return strconv.Atoi(env)
 }
 
 func EnvInt8(key, defaultValue string) (int8, error) {
-	value, err := strconv.ParseInt(Env(key, defaultValue), 10, 8)
+	env := Env(key, defaultValue)
+	if env == "" {
+		return 0, nil
+	}
+	value, err := strconv.ParseInt(env, 10, 8)
 	if err != nil {
 		return 0, err
 	}
@@ -38,7 +169,11 @@ func EnvInt8(key, defaultValue string) (int8, error) {
 }
 
 func EnvInt16(key, defaultValue string) (int16, error) {
-	value, err := strconv.ParseInt(Env(key, defaultValue), 10, 16)
+	env := Env(key, defaultValue)
+	if env == "" {
+		return 0, nil
+	}
+	value, err := strconv.ParseInt(env, 10, 16)
 	if err != nil {
 		return 0, err
 	}
@@ -46,7 +181,11 @@ func EnvInt16(key, defaultValue string) (int16, error) {
 }
 
 func EnvInt32(key, defaultValue string) (int32, error) {
-	value, err := strconv.ParseInt(Env(key, defaultValue), 10, 32)
+	env := Env(key, defaultValue)
+	if env == "" {
+		return 0, nil
+	}
+	value, err := strconv.ParseInt(env, 10, 32)
 	if err != nil {
 		return 0, err
 	}
@@ -54,12 +193,20 @@ func EnvInt32(key, defaultValue string) (int32, error) {
 }
 
 func EnvInt64(key, defaultValue string) (int64, error) {
-	return strconv.ParseInt(Env(key, defaultValue), 10, 64)
+	env := Env(key, defaultValue)
+	if env == "" {
+		return 0, nil
+	}
+	return strconv.ParseInt(env, 10, 64)
 }
 
 // EnvUint Uint
 func EnvUint(key, defaultValue string) (uint, error) {
-	value, err := strconv.ParseUint(Env(key, defaultValue), 10, 0)
+	env := Env(key, defaultValue)
+	if env == "" {
+		return 0, nil
+	}
+	value, err := strconv.ParseUint(env, 10, 0)
 	if err != nil {
 		return 0, err
 	}
@@ -67,7 +214,11 @@ func EnvUint(key, defaultValue string) (uint, error) {
 }
 
 func EnvUint8(key, defaultValue string) (uint8, error) {
-	value, err := strconv.ParseUint(Env(key, defaultValue), 10, 8)
+	env := Env(key, defaultValue)
+	if env == "" {
+		return 0, nil
+	}
+	value, err := strconv.ParseUint(env, 10, 8)
 	if err != nil {
 		return 0, err
 	}
@@ -75,7 +226,11 @@ func EnvUint8(key, defaultValue string) (uint8, error) {
 }
 
 func EnvUint16(key, defaultValue string) (uint16, error) {
-	value, err := strconv.ParseUint(Env(key, defaultValue), 10, 16)
+	env := Env(key, defaultValue)
+	if env == "" {
+		return 0, nil
+	}
+	value, err := strconv.ParseUint(env, 10, 16)
 	if err != nil {
 		return 0, err
 	}
@@ -83,7 +238,11 @@ func EnvUint16(key, defaultValue string) (uint16, error) {
 }
 
 func EnvUint32(key, defaultValue string) (uint32, error) {
-	value, err := strconv.ParseUint(Env(key, defaultValue), 10, 32)
+	env := Env(key, defaultValue)
+	if env == "" {
+		return 0, nil
+	}
+	value, err := strconv.ParseUint(env, 10, 32)
 	if err != nil {
 		return 0, err
 	}
@@ -91,12 +250,20 @@ func EnvUint32(key, defaultValue string) (uint32, error) {
 }
 
 func EnvUint64(key, defaultValue string) (uint64, error) {
-	return strconv.ParseUint(Env(key, defaultValue), 10, 64)
+	env := Env(key, defaultValue)
+	if env == "" {
+		return 0, nil
+	}
+	return strconv.ParseUint(env, 10, 64)
 }
 
 // EnvFloat32 Float
 func EnvFloat32(key, defaultValue string) (float32, error) {
-	value, err := strconv.ParseFloat(Env(key, defaultValue), 32)
+	env := Env(key, defaultValue)
+	if env == "" {
+		return 0, nil
+	}
+	value, err := strconv.ParseFloat(env, 32)
 	if err != nil {
 		return 0, err
 	}
@@ -104,10 +271,18 @@ func EnvFloat32(key, defaultValue string) (float32, error) {
 }
 
 func EnvFloat64(key, defaultValue string) (float64, error) {
-	return strconv.ParseFloat(Env(key, defaultValue), 64)
+	env := Env(key, defaultValue)
+	if env == "" {
+		return 0, nil
+	}
+	return strconv.ParseFloat(env, 64)
 }
 
 // EnvBool Boolean
 func EnvBool(key, defaultValue string) (bool, error) {
-	return strconv.ParseBool(Env(key, defaultValue))
+	env := Env(key, defaultValue)
+	if env == "" {
+		return false, nil
+	}
+	return strconv.ParseBool(env)
 }
