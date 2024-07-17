bfc68c7eeef2127c078b40a37bef6190bd394645
trinhdaiphuc <trinhdaiphuc@gmail.com>
2024-07-17 16:50:46 +0700

    #chore: update unit test

--- examples/example.go
+++ examples/example.go
@@ -10,13 +10,14 @@ import (
 )
 
 type Config struct {
-	Host    string        `env:"HOST,default=localhost"`
-	Port    int           `env:"PORT,default=8080"`
-	Bytes   []byte        `env:"ENV_BYTES,default=foo"`
-	Float32 float32       `env:"ENV_FLOAT,default=12.34"`
-	Timeout time.Duration `env:"TIMEOUT"`
-	Date    time.Time     `env:"ENV_DATE"`
-	None    string
+	Host      string        `env:"HOST,default=localhost"`
+	Port      int           `env:"PORT,default=8080"`
+	Bytes     []byte        `env:"ENV_BYTES,default=foo"`
+	Float32   float32       `env:"ENV_FLOAT,default=12.34"`
+	Timeout   time.Duration `env:"TIMEOUT"`
+	Date      time.Time     `env:"ENV_DATE"`
+	Addresses []string      `env:"ADDRESSES"`
+	None      string
 }
 
 func main() {
